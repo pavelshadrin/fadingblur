da2687cee086097024a7983183b39d47c5905e40
Pavel Shadrin <pdshadrin@gmail.com>
2023-09-29 23:38:14 +0100

    Initial commit (standard project boilerplate)

---
+++ fadingblur/fadingblur/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fadingblur
+//
+//  Created by Pavel Shadrin on 29.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
